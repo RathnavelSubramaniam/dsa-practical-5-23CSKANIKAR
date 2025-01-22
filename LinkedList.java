935d3a451327705a76aedd0601ff0ca222b315b7
23CSAKANIKAR <139537768+23CSKANIKAR@users.noreply.github.com>
2025-01-22 06:35:32 +0000

    SUBMIT5

--- LinkedList.java
+++ LinkedList.java
@@ -39,11 +39,17 @@ scanner.close();
     }
 int length() 
     {
-  //Type your code here
+      int count=0;
+      Node current=head;
+      while(current !=null){
+        count++;
+        current= current.next;
+    }
+    return count;
     }
 public void displayList() {
        Node current = head;
-        while (current != null) {
+        while (current!=null) {
 System.out.print(current.data + " -> ");
             current = current.next;
         }
